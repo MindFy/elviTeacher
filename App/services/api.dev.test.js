945e00b6c4e0cfba5d5a38da330c8edbccaa0f42
Song Li <song.li@d3j.io>
2018-05-08 09:23:22 +0800

    add websocket api test

--- App/services/api.dev.test.js
+++ App/services/api.dev.test.js
@@ -1,5 +1,9 @@
-import { API_ROOT } from './api.dev'
+import { API_ROOT, ws } from './api.dev'
 
 test('API_ROOT should be http://127.0.0.1:8080', () => {
   expect(API_ROOT).toBe('http://127.0.0.1:8080')
+})
+
+test('ws should be ws://127.0.0.1:8080/1.0/push', () => {
+  expect(ws).toBe('ws://127.0.0.1:8080/1.0/push')
 })
