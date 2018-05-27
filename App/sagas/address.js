d3563e9fc2fbb9aadf810912997dd8b488e984b5
mengqiu.zhuang <mengqiu.zhuang@foxmail.com>
2018-05-27 20:16:58 +0800

    refactor saga and add more test

--- App/sagas/address.js
+++ App/sagas/address.js
@@ -9,37 +9,37 @@ import * as api from '../services/api'
 import { common } from '../constants/common'
 
 /* 增加一个提币地址 */
-export function* addAddress() {
-  while (true) {
-    const request = yield take(constants.ADD_REQUEST)
-    const response = yield call(api.add, request.data)
-    if (response.success) {
-      // Toast.success('添加成功')
-      yield put({ type: constants.ADD_SUCCEED, response })
-    } else {
-      yield put({ type: constants.ADD_FAILED, response })
-      if (response.error.message === common.badNet) {
-        // Toast.fail('网络连接失败，请稍后重试')
-      } else if (response.error.code === 4000411) {
-        // Toast.fail('货币或地址有误')
-      } else if (response.error.code === 4000412) {
-        // Toast.fail('货币不存在')
-      } else if (response.error.code === 4000413) {
-        // Toast.fail('提现地址长度有误，请输入正确的提现地址')
-      } else if (response.error.code === 4000414) {
-        // Toast.fail('地址已存在')
-      } else if (response.error.code === 4000415) {
-        // Toast.fail('仅支持BTC、ETH添加地址')
-      } else if (response.error.code === 4000416) {
-        // Toast.fail('无效的提现地址')
-      } else if (response.error.code === 4000156) {
-        // Toast.fail('授权验证失败')
-      } else {
-        // Toast.fail('添加失败')
-      }
-    }
-  }
-}
+// export function* addAddress() {
+//   while (true) {
+//     const request = yield take(constants.ADD_REQUEST)
+//     const response = yield call(api.add, request.data)
+//     if (response.success) {
+//       // Toast.success('添加成功')
+//       yield put({ type: constants.ADD_SUCCEED, response })
+//     } else {
+//       yield put({ type: constants.ADD_FAILED, response })
+//       if (response.error.message === common.badNet) {
+//         // Toast.fail('网络连接失败，请稍后重试')
+//       } else if (response.error.code === 4000411) {
+//         // Toast.fail('货币或地址有误')
+//       } else if (response.error.code === 4000412) {
+//         // Toast.fail('货币不存在')
+//       } else if (response.error.code === 4000413) {
+//         // Toast.fail('提现地址长度有误，请输入正确的提现地址')
+//       } else if (response.error.code === 4000414) {
+//         // Toast.fail('地址已存在')
+//       } else if (response.error.code === 4000415) {
+//         // Toast.fail('仅支持BTC、ETH添加地址')
+//       } else if (response.error.code === 4000416) {
+//         // Toast.fail('无效的提现地址')
+//       } else if (response.error.code === 4000156) {
+//         // Toast.fail('授权验证失败')
+//       } else {
+//         // Toast.fail('添加失败')
+//       }
+//     }
+//   }
+// }
 
 export function* fetchAddress(action) {
   const { schema } = action
@@ -52,6 +52,20 @@ export function* fetchAddress(action) {
   }
 }
 
+export function* addAddress_(action) {
+  const response = yield call(api.add, action.data)
+
+  if (response.success) {
+    yield put({ type: constants.ADD_SUCCEED, response })
+  } else {
+    yield put({ type: constants.ADD_FAILED, response })
+  }
+}
+
 export function* findAddress() {
   yield takeEvery(constants.FIND_ADDRESS_REQUEST, fetchAddress)
+}
+
+export function* addAddress() {
+  yield takeEvery(constants.ADD_REQUEST, addAddress_)
 }
--- App/sagas/address.spec.js
+++ App/sagas/address.spec.js
@@ -1,11 +1,11 @@
 import { takeEvery, call, put } from 'redux-saga/effects'
-import { findAddress, addAddress, fetchAddress } from './address'
+import { findAddress, addAddress, addAddress_, fetchAddress } from './address'
 import schema from '../schemas/address'
 import * as api from '../services/api'
 import * as constants from '../constants/index'
 
 
-it('watch findAddress saga', () => {
+it('findAddress watcher', () => {
   const it = findAddress()
   expect(it.next().value).toEqual(takeEvery(constants.FIND_ADDRESS_REQUEST, fetchAddress))
   expect(it.next().done).toBe(true)
@@ -36,5 +36,49 @@ it('fetchAddress failed', () => {
 
   expect(it.next().value).toEqual(call(api.graphql, schema(1)))
   expect(it.next(response).value).toEqual(put({ type: constants.FIND_ADDRESS_FAILED, response }))
+  expect(it.next().done).toBe(true)
+})
+
+it('addAddress watcher', () => {
+  const it = addAddress()
+  expect(it.next().value).toEqual(takeEvery(constants.ADD_REQUEST, addAddress_))
+  expect(it.next().done).toBe(true)
+})
+
+it('addAddress success', () => {
+  const action = {
+    data: {
+      token_id: 2,
+      withdrawaddr: '18cBEMRxXHqzWWCxZNtU91F5sbUNKhL5PX',
+      remark: 'test',
+      code: '1234',
+    },
+  }
+  const response = {
+    success: true,
+  }
+  const it = addAddress_(action)
+
+  expect(it.next().value).toEqual(call(api.add, action.data))
+  expect(it.next(response).value).toEqual(put({ type: constants.ADD_SUCCEED, response }))
+  expect(it.next().done).toBe(true)
+})
+
+it('addAddress success', () => {
+  const action = {
+    data: {
+      token_id: 2,
+      withdrawaddr: '18cBEMRxXHqzWWCxZNtU91F5sbUNKhL5PX',
+      remark: 'test',
+      code: '1234',
+    },
+  }
+  const response = {
+    success: false,
+  }
+  const it = addAddress_(action)
+
+  expect(it.next().value).toEqual(call(api.add, action.data))
+  expect(it.next(response).value).toEqual(put({ type: constants.ADD_FAILED, response }))
   expect(it.next().done).toBe(true)
 })
