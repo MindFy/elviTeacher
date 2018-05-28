9e424865b8a1ebfc2a8489805df74a60973333db
chenxj <xuejiao.chen@d3j.io>
2018-05-28 09:34:23 +0800

    fix 新交易

--- App/reducers/detailDeal.js
+++ App/reducers/detailDeal.js
@@ -9,6 +9,14 @@ const initialState = {
 export default function detailDeal(state = initialState, action) {
   let nextState = state
   switch (action.type) {
+    case constants.TEXTINPUT_DELEGATE_UPDATE:
+      nextState = {
+        ...state,
+        price: action.data.price,
+        quantity: action.data.quantity,
+        amount: action.data.amount,
+      }
+      break
     case constants.DETAIL_DEAL_UPDATEKV:
       nextState = {
         ...state,
