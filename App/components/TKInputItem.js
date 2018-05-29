ba02018fdc1456cb302b7ed3e9c42b16d517a2e1
chenxj <xuejiao.chen@d3j.io>
2018-05-29 18:27:48 +0800

    add inputItem

---
+++ App/components/TKInputItem.js
@@ -0,0 +1,155 @@
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+} from 'react-native'
+import { common } from '../constants/common'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    height: common.h40,
+
+    backgroundColor: common.navBgColor,
+    borderColor: common.borderColor,
+    borderRadius: 1,
+    borderWidth: 1,
+  },
+  titleContainer: {
+    marginLeft: common.margin10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    color: common.textColor,
+    fontSize: common.font12,
+  },
+  inputContainer: {
+    flex: 1,
+    marginHorizontal: common.margin10,
+    alignSelf: 'center',
+  },
+  input: {
+    color: common.textColor,
+    fontSize: common.font12,
+  },
+  extraContainer: {
+    marginRight: common.margin10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  extra: {
+    color: common.textColor,
+    fontSize: common.font12,
+  },
+})
+
+class TKInputItem extends Component {
+  onChangeText = (text) => {
+    if (this.props.onTextChanged) {
+      this.props.onChangeText(text)
+    }
+  }
+
+  onChange = (e) => {
+    if (this.props.onChange) {
+      this.props.onChange(e)
+    }
+  }
+
+  clear = () => {
+    this.textInputRef.clear()
+  }
+
+  renderTitle = () => {
+    let titleView = null
+    if (this.props.renderTitle) {
+      titleView = this.props.renderTitle()
+    } else if (!this.props.title) {
+      return null
+    } else {
+      titleView = (
+        <Text style={[styles.title, this.props.titleStyle]}>
+          {this.props.title}
+        </Text>
+      )
+    }
+    return (
+      <View style={styles.titleContainer}>
+        {titleView}
+      </View>
+    )
+  }
+
+  renderInput = () => {
+    const {
+      value,
+      defaultValue,
+      placeholder,
+      inputStyle,
+      maxLenth,
+      keyboardType,
+      editable,
+    } = this.props
+    if (this.props.renderInput) {
+      return this.props.renderInput()
+    }
+    return (
+      <TextInput
+        ref={(e) => { this.textInputRef = e }}
+        style={[styles.inputContainer, styles.input, inputStyle]}
+        autoCapitalize="none"
+        clearButtonMode="while-editing"
+        value={value}
+        defaultValue={defaultValue}
+        placeholder={placeholder}
+        placeholderTextColor={common.placeholderColor}
+        keyboardType={keyboardType}
+        maxLength={maxLenth}
+        onChange={e => this.onChange(e)}
+        onChangeText={text => this.onChangeText(text)}
+        editable={editable}
+        enablesReturnKeyAutomatically
+        underlineColorAndroid="transparent"
+        {...this.props.textInputProps}
+      />
+    )
+  }
+
+  renderExtra = () => {
+    let extraView = null
+    if (this.props.renderExtra) {
+      extraView = this.props.renderExtra()
+    } else if (!this.props.extra) {
+      return extraView
+    } else {
+      extraView = (
+        <Text style={[styles.extra, this.props.extraStyle]}>
+          {this.props.extra}
+        </Text>
+      )
+    }
+    return (
+      <View style={styles.extraContainer}>
+        {extraView}
+      </View>
+    )
+  }
+
+  render() {
+    return (
+      <View style={[styles.container, this.props.viewStyle]}>
+        {this.renderTitle()}
+
+        {this.renderInput()}
+
+        {this.renderExtra()}
+      </View>
+    )
+  }
+}
+
+export default TKInputItem
