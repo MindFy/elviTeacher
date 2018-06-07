a461b6d5dcc3b5dbd0898850c3419b51703058d5
chenxj <xuejiao.chen@d3j.io>
2018-06-07 18:46:52 +0800

    fix 当前委托 颜色

--- App/views/transactions/Orders.js
+++ App/views/transactions/Orders.js
@@ -511,7 +511,7 @@ class Orders extends Component {
     const goodsCurrency = `${goodsName}/${currencyName}`
     const buySell = item.direct === 'buy' ? '买入' : '卖出'
     const buySellColor =
-      item.direct === 'sell' ? { color: common.greenColo } : { color: common.redColor }
+      item.direct === 'sell' ? { color: common.greenColor } : { color: common.redColor }
     const cancelColor =
       cancelDisabled ? { color: common.placeholderColor } : { color: common.btnTextColor }
 
