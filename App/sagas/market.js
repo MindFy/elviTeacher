6cdc3817abd1a29be0d5b0017e34dc46187c7dd8
chenxj <xuejiao.chen@d3j.io>
2018-07-17 14:40:07 +0800

    fix 自选修改

--- App/sagas/market.js
+++ App/sagas/market.js
@@ -12,47 +12,9 @@ const getFavoriteList = state => state.market.favoriteList
 export function* requestPairInfoWorker(action) {
   const { payload } = action
   const response = yield call(api.getRose, payload)
-  const tmepMarkedTokens = []
 
   if (response.success) {
-    const result = {}
-    const favoriteList = yield select(getFavoriteList)
-    for (let i = 0; i < response.result.length; i++) {
-      const ones = response.result[i]
-      const tempTokens = []
-      for (let j = 0; j < ones.sub.length; j++) {
-        const one = ones.sub[j]
-        const obj = {
-          goods: {
-            id: one.id,
-            name: one.name,
-          },
-          currency: {
-            id: ones.id,
-            name: ones.name,
-          },
-          cprice: one.cprice,
-          hprice: one.hprice,
-          lastprice: one.lastprice,
-          lprice: one.lprice,
-          quantity: one.quantity,
-          rose: one.rose,
-        }
-        if (favoriteList &&
-          Object.keys(favoriteList).some(key => key === `${one.id}_${ones.id}`)) {
-          const newObj = { ...obj, isFavorited: true }
-          tempTokens.push(newObj)
-          tmepMarkedTokens.push(newObj)
-        } else {
-          tempTokens.push(obj)
-        }
-        if (j === ones.sub.length - 1) {
-          result[ones.name] = tempTokens
-        }
-      }
-      result.markedTokens = tmepMarkedTokens
-    }
-
+    const result = response.result
     yield put({
       type: 'withdraw/request_pair_info_succeed',
       payload: result,
@@ -95,28 +57,9 @@ function* setFavoriteAdd(action) {
       type: 'market/get_favorite_success',
       payload: dict,
     })
-
-    const { currency, goods } = payload
-    const pairs = yield select(state => state.market.pairs)
-    const newPairs = { ...pairs }
-    const ones = newPairs[currency.name]
-    for (let i = 0; i < ones.length; i++) {
-      const one = ones[i]
-      if (one.goods.id === goods.id && one.currency.id === currency.id) {
-        one.isFavorited = true
-        const temp = newPairs.markedTokens || []
-        temp.push(one)
-        newPairs.markedTokens = temp
-        break
-      }
-    }
-    yield put({
-      type: 'market/set_favorite_success',
-      payload: newPairs,
-    })
   } else {
     yield put({
-      type: 'market/set_favorite_failed',
+      type: 'market/get_favorite_failed',
     })
   }
 }
@@ -132,33 +75,9 @@ function* setFavoriteRemove(action) {
       type: 'market/get_favorite_success',
       payload: dict,
     })
-
-    const { currency, goods } = payload
-    const pairs = yield select(state => state.market.pairs)
-    const newPairs = { ...pairs }
-    const ones = newPairs[currency.name]
-    for (let i = 0; i < ones.length; i++) {
-      const one = ones[i]
-      if (one.goods.id === goods.id && one.currency.id === currency.id) {
-        one.isFavorited = false
-        const temp = newPairs.markedTokens || []
-        for (let j = 0; j < temp.length; j++) {
-          const tempOne = temp[j]
-          if (tempOne.goods.id === one.goods.id) {
-            temp.splice(j, 1)
-            newPairs.markedTokens = temp
-            break
-          }
-        }
-      }
-    }
-    yield put({
-      type: 'market/set_favorite_success',
-      payload: newPairs,
-    })
   } else {
     yield put({
-      type: 'market/set_favorite_failed',
+      type: 'market/get_favorite_failed',
     })
   }
 }
