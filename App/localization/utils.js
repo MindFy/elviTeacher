471753752b5c4aa6888baef10fb93c0ddd46b790
fuck <fuck@ww1deMacBook-Air.local>
2018-08-02 11:54:05 +0800

    feature 多语言

--- App/localization/utils.js
+++ App/localization/utils.js
@@ -1,17 +1,16 @@
 export default function (language, key) {
   let localizationJSON = {}
   switch (language) {
-    case 'en':
-      localizationJSON = require('./localization_en.json')
+    case 'zh_hans':
+      localizationJSON = require('./localization_zh_cn.json')
       break
-    case 'zh_hk':
-      localizationJSON = require('./localization_zh_hk.json')
-      break
-    case 'zh_tw':
+    case 'zh_hant':
       localizationJSON = require('./localization_zh_tw.json')
       break
+    case 'ja':
+    case 'ko':
     default:
-      localizationJSON = require('./localization_zh_cn.json')
+      localizationJSON = require('./localization_en.json')
       break
   }
   return localizationJSON[key] || ''
