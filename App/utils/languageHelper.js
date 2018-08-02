f0aada4902d50adb37ce2af8a9c4c70fe831f8c1
fuck <fuck@ww1deMacBook-Air.local>
2018-08-02 11:54:05 +0800

    feature 多语言

--- App/utils/languageHelper.js
+++ App/utils/languageHelper.js
@@ -17,10 +17,18 @@ export const getDefaultLanguage = () => {
     return fs.userDefaultSysLang
   }
 
-  let defaultLanguage
-  const evt = deviceInfo.getDeviceLocale()
-  if (evt.indexOf('zh') > -1) {
-    defaultLanguage = 'zh_cn'
+  let defaultLanguage = 'en'
+  const evt = deviceInfo.getDeviceLocale().toLocaleLowerCase()
+  // en(英文) zh-Hans-US(简体) zh-Hant-US(繁体) ja-US(日文) ja-US(韩文) 【IOS】
+  // zh-Hans-CN(简体)  zh-Hant-TW(繁体) en-GB(英文) ja-JP(日本) ko-KR(韩文) 【ANDROID】
+  if (evt.indexOf('zh-hans-') > -1) {
+    defaultLanguage = 'zh_hans'
+  } else if (evt.indexOf('zh-hant-') > -1) {
+    defaultLanguage = 'zh_hant'
+  } else if (evt.indexOf('ja-') > -1) {
+    defaultLanguage = 'ja'
+  } else if (evt.indexOf('ko-') > -1) {
+    defaultLanguage = 'ko'
   } else {
     defaultLanguage = 'en'
   }
